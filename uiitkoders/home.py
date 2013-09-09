054192a85cf1887c67acdf2031e7da619769d17b
himalayan786 <kunaluiit@hotmail.com>
2013-09-09 23:07:47 +0530

    UIITKODERS' website

---
+++ uiitkoders/home.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import webapp2
+import jinja2
+import os
+jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)+'/templates'))
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+	template_values={}
+	template = jinja_environment.get_template('home.html')
+	self.response.out.write(template.render(template_values))
+
+app = webapp2.WSGIApplication([
+    ('/.*', MainHandler)
+], debug=True)
